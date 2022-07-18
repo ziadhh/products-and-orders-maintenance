5cc7e2902392867597f40e443537c5aefe9058cd
Ziad Hammad <ziadhh@test.com>
2022-07-19 01:39:49 +0200

    first update

---
+++ src/main/java/com/rohlik/productsandordersmaintenance/ProductsAndOrdersMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.rohlik.productsandordersmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsAndOrdersMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsAndOrdersMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohlik/productsandordersmaintenance/ProductsAndOrdersMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohlik.productsandordersmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsAndOrdersMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
