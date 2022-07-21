231daac6b0c696d8a35787697f3270df7fc09983
Ziad Hammad <ziadhh@test.com>
2022-07-22 01:30:30 +0200

    update

--- src/main/java/com/rohlik/productsandordersmaintenance/ProductsAndOrdersMaintenanceApplication.java
+++ src/main/java/com/rohlik/productsandordersmaintenance/ProductsAndOrdersMaintenanceApplication.java
@@ -3,8 +3,10 @@ package com.rohlik.productsandordersmaintenance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProductsAndOrdersMaintenanceApplication {
 
 	public static void main(String[] args) {
